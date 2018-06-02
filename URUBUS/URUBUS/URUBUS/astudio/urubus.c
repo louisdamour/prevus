2c3c1cd26312ba89549e6f662b6d0d76037b2da5
Louis D'Amour <louis.damour@usherbrooke.ca>
2018-06-02 15:39:22 -0400

    Ajouter des fonctions pour contrôler la couleur d'une DEL RGB.

--- URUBUS/URUBUS/URUBUS/astudio/urubus.c
+++ URUBUS/URUBUS/URUBUS/astudio/urubus.c
@@ -1,78 +1,28 @@
-/**
- * \file template.c
- *
- * \brief Empty application template
- *
- * Copyright (C) 2012-2014, Atmel Corporation. All rights reserved.
- *
- * \asf_license_start
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. The name of Atmel may not be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * 4. This software may only be redistributed and used in connection with an
- *    Atmel microcontroller product.
- *
- * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
- * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * \asf_license_stop
- *
- * Modification and other use of this code is subject to Atmel's Limited
- * License Agreement (license.txt).
- *
- * $Id: template.c 9267 2014-03-18 21:46:19Z ataradov $
- *
- */
-
 /*- Includes ---------------------------------------------------------------*/
+
 #include "sys.h"
 #include "phy.h"
 
-/*- Definitions ------------------------------------------------------------*/
-// Put your preprocessor definitions here
-
-/*- Types ------------------------------------------------------------------*/
-// Put your type definitions here
-
 /*- Prototypes -------------------------------------------------------------*/
-// Put your function prototypes here
+
 char Lis_UART(void);
 void Ecris_UART(char data);
 void init_UART(void);
 void SYS_Init(void);
+void red(void);
+void magenta(void);
+void cyan(void);
+void yellow(void);
+void off(void);
 
 /*- Variables --------------------------------------------------------------*/
-// Put your variables here
+
 uint8_t receivedWireless;	//cette variable deviendra 1 lorsqu'un nouveau paquet aura été recu via wireless (et copié dans "PHY_DataInd_t ind"
 							//il faut la mettre a 0 apres avoir géré le paquet; tout message recu via wireless pendant que cette variable est a 1 sera jeté
 
 PHY_DataInd_t ind; //cet objet contiendra les informations concernant le dernier paquet qui vient de rentrer
 
-
-/*- Implementations --------------------------------------------------------*/
-
-// Put your function implementations here
-
+// IMPLEMENTATION
 /*************************************************************************//**
 *****************************************************************************/
 static void APP_TaskHandler(void)
@@ -91,10 +41,6 @@ static void APP_TaskHandler(void)
 		}
   }
 
-
-
-
-  
   if(receivedWireless == 1) //est-ce qu'un paquet a été recu sur le wireless? 
   {
 	char buf[196];
@@ -127,9 +73,8 @@ static void APP_TaskHandler(void)
   }
 }
 
-
-
-/*************************************************************************//**
+//MAIN
+/*****************************************************************************
 *****************************************************************************/
 int main(void)
 {
@@ -142,14 +87,8 @@ int main(void)
   }
 }
 
-
-
-
-
-
-
 //FONCTION D'INITIALISATION
-/*************************************************************************//**
+/*****************************************************************************
 *****************************************************************************/
 void SYS_Init(void)
 {
@@ -158,35 +97,20 @@ wdt_disable();
 init_UART();
 PHY_Init(); //initialise le wireless
 PHY_SetRxState(1); //TRX_CMD_RX_ON
+	
+	DPDS0 = 0x1; // 4 mA
+	DDRB = 0xE; //PB1, PB2, PB3 output
+	PORTB |= 0xE; //Turn off LED
+	off();
 }
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //FONCTIONS POUR L'UART
-
+/*****************************************************************************
+*****************************************************************************/
 char Lis_UART(void)
 {
 
 char data = 0; 
-
 
 	if(UCSR1A & (0x01 << RXC1))
 	{
@@ -196,13 +120,11 @@ char data = 0;
 return data;
 }
 
-
 void Ecris_UART(char data)
 {
 	UDR1 = data;
 	while(!(UCSR1A & (0x01 << UDRE1)));
 }
-
 
 void init_UART(void)
 {
@@ -217,3 +139,35 @@ void init_UART(void)
 }
 
 
+//FONCTIONS POUR RGB
+/*****************************************************************************
+*****************************************************************************/
+
+void red()
+{
+	PORTB |= 0x2;
+	PORTB &= ~0xC; 
+}
+
+void magenta()
+{
+	PORTB |= 0xA;
+	PORTB &= ~0x4;
+}
+
+void cyan()
+{
+	PORTB |= 0xC;
+	PORTB &= ~0x2;
+}
+
+void yellow()
+{
+	PORTB |= 0x6;
+	PORTB &= ~0x8;
+}
+
+void off()
+{
+	PORTB &= ~0xE;
+}
